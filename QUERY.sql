77152fa11010edb3b5bcb9fc7610b7ec7afe526d
Shahid Hasan Shovu <shahid@softvence.com>
2026-06-14 13:02:45 +0600

    feat: implement Query 5 in QUERY.sql

--- QUERY.sql
+++ QUERY.sql
@@ -119,3 +119,8 @@ SELECT b.booking_id, u.full_name, m.fixture, b.total_cost
 FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id
 INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+SELECT u.user_id, u.full_name, b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
